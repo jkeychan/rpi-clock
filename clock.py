85ef7fba52bfb0fad37da9943ad8ddfb739ffbff
jkeychan <jkeychan@ibiblio.org>
2023-08-30 13:55:05 -0400

    refactored clock.py to be more efficient

--- clock.py
+++ clock.py
@@ -1,77 +1,17 @@
-#!/usr/bin/python3
-
 import os
 import time
 import board
 import busio
 import adafruit_ht16k33.segments as segments
 import requests
-import json
 import configparser
 import ntplib
 
-# Check if custom_chars.py exists, and if not, create a new one with default values
-CUSTOM_CHARS_FILE = 'custom_chars.py'
-DEFAULT_CUSTOM_CHARS = {
-    'custom_chars': {
-        'char1': [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-        'char2': [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-    }
-}
-
-if not os.path.exists(CUSTOM_CHARS_FILE):
-    print("No custom_chars.py file detected, creating custom_chars.py...")
-    with open(CUSTOM_CHARS_FILE, 'w') as custom_chars_file:
-        custom_chars_file.write("custom_chars = {\n")
-        for char_name, char_values in DEFAULT_CUSTOM_CHARS['custom_chars'].items():
-            custom_chars_file.write(f"    '{char_name}': {char_values},\n")
-        custom_chars_file.write("}\n")
-
-# Import the custom_chars.py module after ensuring it exists
-
-# Helper function to display custom characters
-
-
-def display_custom_chars(chars):
-    display.print(chars)
-
-# Helper function to display the temperature on the display
-
-
-def display_temperature(temp, unit):
-    temp_str = "{:.0f} {}".format(temp, unit)
-    display.print(temp_str.rjust(7))
-
-# Helper function to display the time on the display
-
-
-def display_time():
-    now = time.localtime()
-    hour = now.tm_hour
-
-    # Check the time format from the config file
-    time_format = config.get('Display', 'TIME_FORMAT')
-
-    if time_format == '12':
-        # Convert to 12-hour format
-        hour = hour % 12
-        if hour == 0:
-            hour = 12
-
-    minute = now.tm_min
-
-    time_str = "{:2d}{:02d}".format(hour, minute)
-    display.print(time_str)
-
-# Helper function to display the humidity on the display
-
-
-def display_humidity(humidity):
-    humidity_str = "{:02d}".format(round(humidity))
-    display.print("rh{}".format(humidity_str))
-
-
+API_REFRESH_CYCLES = 10
+cached_weather_info = None
+api_endpoint = "https://api.openweathermap.org/data/2.5/weather"
 CONFIG_FILE = 'config.ini'
+
 DEFAULT_CONFIG = {
     'Weather': {
         'API_KEY': 'your_api_key_here',
@@ -83,72 +23,78 @@ DEFAULT_CONFIG = {
     },
     'NTP': {
         'PREFERRED_SERVER': '127.0.0.1'
+    },
+    'Cycle': {
+        'time_display': '2',
+        'temp_display': '3',
+        'feels_like_display': '3',
+        'humidity_display': '2'
     }
 }
 
-# Check if config.ini exists, and if not, create a new one with default values
-if not os.path.exists(CONFIG_FILE):
-    print("No configuration file detected, creating config.ini...")
-    config = configparser.ConfigParser()
-    config.read_dict(DEFAULT_CONFIG)
-
-    with open(CONFIG_FILE, 'w') as configfile:
-        config.write(configfile)
-
-# Read configuration from config.ini
 config = configparser.ConfigParser()
+config.read_dict(DEFAULT_CONFIG)
 config.read(CONFIG_FILE)
 
-# Check if 'Weather' section exists, and if not, add it with default values
-if 'Weather' not in config:
-    config['Weather'] = DEFAULT_CONFIG['Weather']
+api_key = config['Weather']['API_KEY']
+zip_code = config['Weather']['ZIP_CODE']
+time_format = config['Display']['TIME_FORMAT']
+temp_unit = config['Display']['TEMP_UNIT']
+preferred_ntp_server = config['NTP']['PREFERRED_SERVER']
+time_display = config.getint('Cycle', 'time_display')
+temp_display = config.getint('Cycle', 'temp_display')
+feels_like_display = config.getint('Cycle', 'feels_like_display')
+humidity_display = config.getint('Cycle', 'humidity_display')
 
-# Check if 'ZIP_CODE' option exists in the 'Weather' section, and if not, add it with a default value
-if 'ZIP_CODE' not in config['Weather']:
-    config['Weather']['ZIP_CODE'] = DEFAULT_CONFIG['Weather']['ZIP_CODE']
-
-# Check if 'NTP' section exists, and if not, add it with default values
-if 'NTP' not in config:
-    config['NTP'] = DEFAULT_CONFIG['NTP']
-
-# Check if 'PREFERRED_SERVER' option exists in the 'NTP' section, and if not, add it with a default value
-if 'PREFERRED_SERVER' not in config['NTP']:
-    config['NTP']['PREFERRED_SERVER'] = DEFAULT_CONFIG['NTP']['PREFERRED_SERVER']
-
-# Write the final configuration to the file
-with open(CONFIG_FILE, 'w') as configfile:
-    config.write(configfile)
-
-# Get the configuration values
-api_key = config.get('Weather', 'API_KEY')
-zip_code = config.get('Weather', 'ZIP_CODE')
-time_format = config.get('Display', 'TIME_FORMAT')
-temp_unit = config.get('Display', 'TEMP_UNIT')
-preferred_ntp_server = config.get('NTP', 'PREFERRED_SERVER')
-
-# Initialize I2C bus and the display
 i2c = busio.I2C(board.SCL, board.SDA)
 display = segments.Seg7x4(i2c)
 
-# OpenWeatherMap API endpoint
-api_endpoint = "https://api.openweathermap.org/data/2.5/weather"
 
-# Function to fetch weather data from OpenWeatherMap API
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius temperature to Fahrenheit."""
+    return (celsius * 9/5) + 32
+
+
+def display_temperature(temp, unit):
+    """Display the temperature on the 7-segment display."""
+    temp_str = "{:.0f} {}".format(temp, unit)
+    display.print(temp_str.rjust(7))
+
+
+def display_time():
+    """Display the current time on the 7-segment display."""
+    now = time.localtime()
+    hour = now.tm_hour
+    if time_format == '12':
+        hour = hour % 12
+        if hour == 0:
+            hour = 12
+    minute = now.tm_min
+    time_str = "{:2d}{:02d}".format(hour, minute)
+    display.print(time_str)
+
+
+def display_humidity(humidity):
+    """Display the relative humidity on the 7-segment display."""
+    humidity_str = "rH{:02d}".format(round(humidity))
+    display.print(humidity_str)
 
 
 def fetch_weather(api_endpoint, zip_code, api_key, temp_unit):
+    """Fetch weather information from OpenWeatherMap."""
     try:
         response = requests.get(api_endpoint, params={
             "zip": zip_code, "appid": api_key, "units": "metric"})
         response.raise_for_status()
-        weather_data = json.loads(response.text)
+        weather_data = response.json()
+
         temperature = round(weather_data["main"]["temp"])
         feels_like = round(weather_data["main"]["feels_like"])
         humidity = round(weather_data["main"]["humidity"])
 
         if temp_unit == 'F':
-            temperature = (temperature * 9/5) + 32
-            feels_like = (feels_like * 9/5) + 32
+            temperature = celsius_to_fahrenheit(temperature)
+            feels_like = celsius_to_fahrenheit(feels_like)
 
         return temperature, feels_like, humidity
 
@@ -156,10 +102,9 @@ def fetch_weather(api_endpoint, zip_code, api_key, temp_unit):
         print("Failed to get weather data:", e)
         return None
 
-# Helper function to get the current time from the NTP server or local system time
-
 
 def get_current_time():
+    """Get current time using an NTP server or system time if NTP fails."""
     try:
         ntp_client = ntplib.NTPClient()
         response = ntp_client.request(
@@ -171,57 +116,47 @@ def get_current_time():
         return time.localtime()
 
 
-# Loop to display time and weather alternately
-while True:
-    # Get the current time from the NTP server or local system time
-    current_time = get_current_time()
+def display_metric_with_message(message, display_function, *args, delay=2):
+    """Display a message followed by a metric value on the 7-segment display."""
+    display.fill(0)
+    display.marquee(message, delay=0.2, loop=False)
+    time.sleep(delay)
+    display_function(*args)
+    display.show()
 
-    for _ in range(6):
-        display_time()
-        display.colon = True  # Enable the colon for time display
-        display.show()
-        time.sleep(1)  # Keep the colon enabled for 1 second
-        display.colon = False  # Disable the colon for time display
-        display.show()
-        time.sleep(1)  # Keep the colon disabled for 1 second
 
-    # Fetch weather data from the OpenWeatherMap API
-    weather_info = fetch_weather(api_endpoint, zip_code, api_key, temp_unit)
-
-    # Display weather information if data is available
-    if weather_info:
-        temperature, feels_like, humidity = weather_info
-
-        display.fill(0)  # Clear the display
-
-        display.marquee('Out', delay=0.2, loop=False)
-        time.sleep(2)
-
-        display_temperature(temperature, temp_unit)
-        display.show()
-        time.sleep(5)
-
-        display.marquee('feel', delay=0.2, loop=False)
-        time.sleep(2)
-
-        display_temperature(feels_like, temp_unit)
-        display.show()
-        time.sleep(5)
-
-        display.fill(0)
-        display_humidity(humidity)
-        display.show()
-        time.sleep(3)
-
-        display.fill(0)
-
-    else:
-        # If weather data fetch fails, display the local system time for 5 seconds
-        for _ in range(5):
+def main_loop():
+    """Run the main loop to display time and weather information."""
+    cycle_counter = 0
+    while True:
+        for _ in range(time_display):
             display_time()
-            display.colon = True  # Enable the colon for time display
+            display.colon = True
             display.show()
-            time.sleep(1)  # Keep the colon enabled for 1 second
-            display.colon = False  # Disable the colon for time display
+            time.sleep(1)
+            display.colon = False
             display.show()
-            time.sleep(1)  # Keep the colon disabled for 1 second
+            time.sleep(1)
+
+        if cycle_counter == 0 or cached_weather_info is None:
+            cached_weather_info = fetch_weather(
+                api_endpoint, zip_code, api_key, temp_unit)
+
+        weather_info = cached_weather_info
+
+        if weather_info:
+            temperature, feels_like, humidity = weather_info
+            display_metric_with_message(
+                'Out', display_temperature, temperature, temp_unit)
+            time.sleep(temp_display)
+            display_metric_with_message(
+                'feel', display_temperature, feels_like, temp_unit)
+            time.sleep(feels_like_display)
+            display_humidity(humidity)
+            time.sleep(humidity_display)
+
+        cycle_counter = (cycle_counter + 1) % API_REFRESH_CYCLES
+
+
+if __name__ == "__main__":
+    main_loop()
